9fe265ddf81551f0a9c83a3262c9157149ccdcf5
Hamza Laroussi <HamzaConnection@hotmail.com>
2021-01-28 20:04:47 +0100

    fixed langauge section on iphone 6/7/8 and x

--- main.js
+++ main.js
@@ -1,37 +1,33 @@
 // Get the current year for the copyright
-$('#year').text(new Date().getFullYear());
+$("#year").text(new Date().getFullYear());
 
-$('.port-item').click(function () {
-    $('.collapse').collapse('hide');
+$(".port-item").click(function () {
+  $(".collapse").collapse("hide");
 });
 
-$('#BackToTopBtn').click(function () {
-    window.scroll(0, 0);
-
+$("#BackToTopBtn").click(function () {
+  window.scroll(0, 0);
 });
 
 window.addEventListener("scroll", function () {
-    if (window.pageYOffset > 250) {
-        $('#BackToTopBtn').addClass("active");
-    }
-    else {
-        $('#BackToTopBtn').removeClass("active")
-    }
+  if (window.pageYOffset > 250) {
+    $("#BackToTopBtn").addClass("active");
+  } else {
+    $("#BackToTopBtn").removeClass("active");
+  }
 });
-
 
 $(window).scroll(function () {
-    if ($(window).scrollTop() + $(window).height() < $(document).height() - $("#main-footer").height()) {
-
-        $('#BackToTopBtn').css("bottom", "20px"); //resetting it
-    }
-    if ($(window).scrollTop() + $(window).height() > $(document).height() - $("#main-footer").height() - 300) {
-        $('#BackToTopBtn').css("bottom", "150px");
-    }
-
+  if (
+    $(window).scrollTop() + $(window).height() <
+    $(document).height() - $("#main-footer").height()
+  ) {
+    $("#BackToTopBtn").css("bottom", "20px"); //resetting it
+  }
+  if (
+    $(window).scrollTop() + $(window).height() >
+    $(document).height() - $("#main-footer").height() - 300
+  ) {
+    $("#BackToTopBtn").css("bottom", "150px");
+  }
 });
-
-
-
-
-
